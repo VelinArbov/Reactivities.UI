211f08e37a182e5c891091f8325cb0fedead037a
Velin Arbov <Velin.Arbov@iris.co.uk>
2023-05-30 16:33:45 +0300

    Create UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import 'semantic-ui-css/semantic.min.css'
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { Button, Header, List } from 'semantic-ui-react';
 
 function App() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect( () => {
+    axios.get('https://localhost:7070/activities').then(response => {
+      setActivities(response.data);
+    })
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Header as='h2' icon='users' content='Reactivities' />
+        <List>
+        {
+          activities.map((activity: any) => (
+            <List.Item key={activity.id}>
+            {activity.title}
+            </List.Item>
+          ))}
+      </List>
+      <Button content="test"/>
     </div>
   );
 }
