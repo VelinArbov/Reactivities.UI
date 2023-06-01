27bf2cd66e470af3094f77f455f91c7516f47b77
Velin Arbov <Velin.Arbov@iris.co.uk>
2023-06-01 14:46:56 +0300

    Creating CRUD  in React

--- src/app/layout/navbar.tsx
+++ src/app/layout/navbar.tsx
@@ -1,19 +1,23 @@
 import React from "react";
-import {Container, Menu, Button} from "semantic-ui-react"
+import { Container, Menu, Button } from "semantic-ui-react"
 
-export default function NavBar(){
-    return(
-<Menu inverted fixed='top'>
-    <Container>
-        <Menu.Item header>
-        < img src="/assets/logo.png" alt="logo" style = {{marginRight: '10px'}}/>
-        Reactivivities
-        </Menu.Item>
-        <Menu.Item name="Activities"/>
-        <Menu.Item>
-            <Button positive content="Create Activity" />
-        </Menu.Item>
-    </Container>
-</Menu>
+interface Props {
+    openForm: () => void;
+}
+
+export default function NavBar({ openForm }: Props) {
+    return (
+        <Menu inverted fixed='top'>
+            <Container>
+                <Menu.Item header>
+                    < img src="/assets/logo.png" alt="logo" style={{ marginRight: '10px' }} />
+                    Reactivivities
+                </Menu.Item>
+                <Menu.Item name="Activities" />
+                <Menu.Item>
+                    <Button onClick={() => openForm()} positive content="Create Activity" />
+                </Menu.Item>
+            </Container>
+        </Menu>
     )
 }
