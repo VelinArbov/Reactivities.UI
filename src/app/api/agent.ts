42c20ea14a600f38bcb95f34495d038eef044be8
Velin Arbov <Velin.Arbov@iris.co.uk>
2023-06-06 15:57:31 +0300

    Handle Errors in  UI

--- src/app/api/agent.ts
+++ src/app/api/agent.ts
@@ -1,9 +1,48 @@
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosError, AxiosResponse } from "axios";
 import { Activity } from "../models/activity";
+import { toast } from "react-toastify";
+import { router } from "../router/Router";
 
 axios.defaults.baseURL = "https://localhost:7070/";
 
 const responseBody = <T>(response: AxiosResponse<T>) => response.data;
+
+axios.interceptors.response.use(
+  async (response) => {
+    return response;
+  },
+  (error: AxiosError) => {
+    const { data, status } = error.response as AxiosResponse;
+    switch (status) {
+      case 400:
+        if (data.errors) {
+          const modalStateErrors = [];
+          for (const key in data.errors) {
+            if (data.errors[key]) {
+              modalStateErrors.push(data.erros[key]);
+            }
+          }
+          throw modalStateErrors.flat();
+        } else {
+          toast.error(data);
+        }
+        break;
+      case 401:
+        toast.error("unauthorised");
+        break;
+      case 403:
+        toast.error("forbidden");
+        break;
+      case 404:
+        router.navigate("/not-founded");
+        break;
+      case 500:
+        toast.error("server error");
+        break;
+    }
+    return Promise.reject(error);
+  }
+);
 
 const requests = {
   get: <T>(url: string) => axios.get<T>(url).then(responseBody),
--- src/app/router/Router.tsx
+++ src/app/router/Router.tsx
@@ -1,9 +1,10 @@
-import { createBrowserRouter, RouteObject } from "react-router-dom";
+import { createBrowserRouter, Navigate, RouteObject } from "react-router-dom";
 import App from "../layout/App";
 import HomePage from "../../features/home/HomePage";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import ActivityForm from "../../features/activities/form/ActivityForm";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
+import NotFound from "../../features/errors/NotFound";
 
 export const routes: RouteObject[] = [
     {
@@ -15,6 +16,8 @@ export const routes: RouteObject[] = [
             { path: 'activities/:id', element: <ActivityDetails /> },
             { path: 'createactivity', element: <ActivityForm key='create' /> },
             { path: 'edit/:id', element: <ActivityForm key='edit' /> },
+            { path: 'not-found', element: <NotFound /> },
+            { path: '*', element: <Navigate replace to='/not-found'/> },
         ]
     }
 ]
