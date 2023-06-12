62e90efcf89a6f9910d09b322974fe9d6cf6005a
Velin Arbov <Velin.Arbov@iris.co.uk>
2023-06-12 18:11:22 +0300

    Fix error handling

--- src/features/users/RegisterForm.tsx
+++ src/features/users/RegisterForm.tsx
@@ -1,42 +1,43 @@
-import { Formik, Form, ErrorMessage } from "formik";
-import TextInput from "../../app/common/form/textInput";
-import { Button, Header, Label } from "semantic-ui-react";
-import { useStore } from "../../app/stores/store";
+import { ErrorMessage, Form, Formik } from "formik";
 import { observer } from "mobx-react-lite";
-import * as Yup from 'yup'
+import { Button, Header } from "semantic-ui-react";
+import { useStore } from "../../app/stores/store";
+import * as Yup from 'yup';
 import ValidationError from "../errors/ValidationError";
+import TextInput from "../../app/common/form/textInput";
 
-export default observer(function RegisterForm() {
+export default observer(function RegsiterForm() {
     const { userStore } = useStore();
     return (
         <Formik
-            initialValues={{ displayName: '', userName: '', email: '', password: '', error: null }}
-            onSubmit={(values, { setErrors }) => userStore.register(values)
-                .catch(error => setErrors({ error }))}
+            initialValues={{ displayName: '', username: '', email: '', password: '', error: null }}
+            onSubmit={(values, { setErrors }) =>
+                userStore.register(values).catch(error => setErrors({ error: error }))}
             validationSchema={Yup.object({
                 displayName: Yup.string().required(),
-                userName: Yup.string().required(),
+                username: Yup.string().required(),
                 email: Yup.string().required(),
                 password: Yup.string().required(),
             })}
         >
             {({ handleSubmit, isSubmitting, errors, isValid, dirty }) => (
-                <Form className='ui form error' onSubmit={handleSubmit} autoComplete="off">
-                    <Header as='h2' content='Sign up to Reactivities' color='teal' textAlign="center" />
-                    <TextInput placeholder="Display Name" name="displayName" label="Display Name" />
-                    <TextInput placeholder="Username" name="username" label="Username" />
-                    <TextInput placeholder="Email" name="email" label="Email" />
-                    <TextInput placeholder="Password" name="password" type='password' label="Password" />
-                    <ErrorMessage name='error' render={() =>
-                       <ValidationError errors={errors.error} /> }
-                       />
+                <Form className='ui form error' onSubmit={handleSubmit} autoComplete='off'>
+                    <Header as='h2' content='Sign up to Reactivities' color="teal" textAlign="center" />
+                    <TextInput placeholder="Display Name" name='displayName' />
+                    <TextInput placeholder="Username" name='username' />
+                    <TextInput placeholder="Email" name='email' />
+                    <TextInput placeholder="Password" name='password' type='password' />
+                    <ErrorMessage name='error' render={() => 
+                        <ValidationError errors={errors.error} />} />
                     <Button
-                        disabled={!isValid  || isSubmitting}
-                        loading={isSubmitting}
-                        positive content='Register'
-                        type="submit" fluid />
+                        disabled={!isValid || !dirty || isSubmitting} 
+                        loading={isSubmitting} 
+                        positive content='Register' 
+                        type="submit" fluid 
+                    />
                 </Form>
             )}
+
         </Formik>
     )
 })
